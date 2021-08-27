32023530daf9a15faf7d183315e6fbcdf92dfe23
David <30283990+DavidTheFrikr@users.noreply.github.com>
2021-08-26 20:09:07 -0700

    Clarify /debug description

--- commands/debug.js
+++ commands/debug.js
@@ -5,5 +5,6 @@ module.exports = {
 		.setName('debug')
 		//covidDateCurrent=("2021-08-15");
 		//function stripped rn and doesn't do anything
-		.setDescription('DEBUG:Sets the date back to 2021-08-15 to force post message in next 10 minutes'),
+		//.setDescription('DEBUG:Sets the date back to 2021-08-15 to force post message in next 10 minutes'),
+		.setDescription('this doesn\'t work, don\'t bother trying to use it lol'),
 };
